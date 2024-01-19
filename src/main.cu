2837babfe08bff71df4f9ef2f64715aef30008c8
Yanwen Xu <xuyanwen2012@gmail.com>
2024-01-19 14:21:21 -0800

    refactor

--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <memory>
 #include <numeric>
 
 #include "common/helper_cuda.hpp"
@@ -31,6 +32,77 @@ const dim3 globalHistDim(laneCount, globalHistWarps, 1);
 // 16x32=512 threads
 const dim3 digitBinDim(laneCount, digitBinWarps, 1);
 
+struct RadixSortData {
+  explicit RadixSortData(const int n) : n(n) {
+    checkCudaErrors(cudaMallocManaged(&u_sort, n * sizeof(unsigned int)));
+    checkCudaErrors(cudaMallocManaged(&u_sort_alt, n * sizeof(unsigned int)));
+    checkCudaErrors(
+        cudaMallocManaged(&u_index, radixPasses * sizeof(unsigned int)));
+    checkCudaErrors(cudaMallocManaged(
+        &u_global_histogram, radix * radixPasses * sizeof(unsigned int)));
+    for (auto& pass_histogram : u_pass_histogram) {
+      checkCudaErrors(cudaMallocManaged(
+          &pass_histogram,
+          radix * binningThreadblocks(n) * sizeof(unsigned int)));
+    }
+  }
+
+  ~RadixSortData() {
+    checkCudaErrors(cudaFree(u_sort));
+    checkCudaErrors(cudaFree(u_sort_alt));
+    checkCudaErrors(cudaFree(u_index));
+    checkCudaErrors(cudaFree(u_global_histogram));
+    for (auto& pass_histogram : u_pass_histogram) {
+      checkCudaErrors(cudaFree(pass_histogram));
+    }
+  }
+
+  [[nodiscard]] bool IsSorted(const int n) const {
+    return std::is_sorted(u_sort, u_sort + n);
+  }
+
+  void InitRandom(const int seed) {
+    constexpr auto block_size = 768;
+    const auto num_blocks = (n + block_size - 1) / block_size;
+    k_InitRandom<<<num_blocks, block_size>>>(u_sort, n, seed);
+    checkCudaErrors(cudaDeviceSynchronize());
+  }
+
+  void DsipatchGlobalHistogram() {
+    k_GlobalHistogram<<<globalHistThreadblocks(n), globalHistDim>>>(
+        u_sort, u_global_histogram, n);
+  }
+
+  void DispatchDigitBinning(const int pass) {
+    if (pass % 2 == 0) {
+      k_DigitBinning<<<binningThreadblocks(n), digitBinDim>>>(
+          u_global_histogram,
+          u_sort,
+          u_sort_alt,
+          u_pass_histogram[pass],
+          u_index,
+          n,
+          pass * 8);
+    } else {
+      k_DigitBinning<<<binningThreadblocks(n), digitBinDim>>>(
+          u_global_histogram,
+          u_sort_alt,
+          u_sort,
+          u_pass_histogram[pass],
+          u_index,
+          n,
+          pass * 8);
+    }
+  }
+
+  int n;
+  unsigned int* u_sort;
+  unsigned int* u_sort_alt;
+  unsigned int* u_index;
+  unsigned int* u_global_histogram;
+  std::array<unsigned int*, radixPasses> u_pass_histogram;
+};
+
 int main(const int argc, const char* argv[]) {
   constexpr int sizeExponent = 28;
   int n = 1 << sizeExponent;  // 256M elements
@@ -39,90 +111,37 @@ int main(const int argc, const char* argv[]) {
     n = std::atoi(argv[1]);
   }
 
+  // check n > 8096, and n is smaller than 2^28
+  if (n < 8096 || n > (1 << 28)) {
+    std::cerr << "n must be between 8096 and 2^28\n";
+    return 1;
+  }
+
   std::cout << "n = " << n << '\n';
 
-  unsigned int* u_sort;
-  unsigned int* u_sort_alt;
-  unsigned int* u_index;
-  unsigned int* u_global_histogram;
-  std::array<unsigned int*, radixPasses> u_pass_histogram;
-
-  std::cout << "allocating...\n";
-
-  checkCudaErrors(cudaMallocManaged(&u_sort, n * sizeof(unsigned int)));
-  checkCudaErrors(cudaMallocManaged(&u_sort_alt, n * sizeof(unsigned int)));
-  checkCudaErrors(
-      cudaMallocManaged(&u_index, radixPasses * sizeof(unsigned int)));
-  checkCudaErrors(cudaMallocManaged(
-      &u_global_histogram, radix * radixPasses * sizeof(unsigned int)));
-  for (auto& pass_histogram : u_pass_histogram) {
-    checkCudaErrors(cudaMallocManaged(
-        &pass_histogram,
-        radix * binningThreadblocks(n) * sizeof(unsigned int)));
-  }
+  auto data_ptr = std::make_unique<RadixSortData>(n);
 
   std::cout << "initializing...\n";
-  {
-    constexpr auto seed = 114514;
-    constexpr auto block_size = 768;
-    const auto num_blocks = (n + block_size - 1) / block_size;
-    k_InitRandom<<<num_blocks, block_size>>>(u_sort, n, seed);
-    checkCudaErrors(cudaDeviceSynchronize());
-  }
+  constexpr auto seed = 114514;
+  data_ptr->InitRandom(seed);
 
-  auto result = std::is_sorted(u_sort, u_sort + n);
+  auto result = data_ptr->IsSorted(n);
   std::cout << "Before sorting: Is sorted ? " << std::boolalpha << result
             << '\n';
 
   std::cout << "start sorting...\n";
-  {
-    k_GlobalHistogram<<<globalHistThreadblocks(n), globalHistDim>>>(
-        u_sort, u_global_histogram, n);
 
-    k_DigitBinning<<<binningThreadblocks(n), digitBinDim>>>(u_global_histogram,
-                                                            u_sort,
-                                                            u_sort_alt,
-                                                            u_pass_histogram[0],
-                                                            u_index,
-                                                            n,
-                                                            0);
+  data_ptr->DsipatchGlobalHistogram();
+  data_ptr->DispatchDigitBinning(0);
+  data_ptr->DispatchDigitBinning(1);
+  data_ptr->DispatchDigitBinning(2);
+  data_ptr->DispatchDigitBinning(3);
 
-    k_DigitBinning<<<binningThreadblocks(n), digitBinDim>>>(u_global_histogram,
-                                                            u_sort_alt,
-                                                            u_sort,
-                                                            u_pass_histogram[1],
-                                                            u_index,
-                                                            n,
-                                                            8);
+  checkCudaErrors(cudaDeviceSynchronize());
 
-    k_DigitBinning<<<binningThreadblocks(n), digitBinDim>>>(u_global_histogram,
-                                                            u_sort,
-                                                            u_sort_alt,
-                                                            u_pass_histogram[2],
-                                                            u_index,
-                                                            n,
-                                                            16);
-
-    k_DigitBinning<<<binningThreadblocks(n), digitBinDim>>>(u_global_histogram,
-                                                            u_sort_alt,
-                                                            u_sort,
-                                                            u_pass_histogram[3],
-                                                            u_index,
-                                                            n,
-                                                            24);
-    checkCudaErrors(cudaDeviceSynchronize());
-  }
-
-  result = std::is_sorted(u_sort, u_sort + n);
+  result = data_ptr->IsSorted(n);
   std::cout << "After sorting: Is sorted ? " << std::boolalpha << result
             << '\n';
 
-  checkCudaErrors(cudaFree(u_sort));
-  checkCudaErrors(cudaFree(u_sort_alt));
-  checkCudaErrors(cudaFree(u_index));
-  checkCudaErrors(cudaFree(u_global_histogram));
-  for (auto& pass_histogram : u_pass_histogram) {
-    checkCudaErrors(cudaFree(pass_histogram));
-  }
   return 0;
 }
--- src/one_sweep.cu
+++ src/one_sweep.cu
@@ -1,39 +1,3 @@
-/******************************************************************************
- * OneSweep
- *
- * Author:  Thomas Smith 9/13/2023
- *
- * Based off of Research by:
- *          Andy Adinets, Nvidia Corporation
- *          Duane Merrill, Nvidia Corporation
- *          https://research.nvidia.com/publication/2022-06_onesweep-faster-least-significant-digit-radix-sort-gpus
- *
- * Copyright (c) 2011, Duane Merrill.  All rights reserved.
- * Copyright (c) 2011-2023, NVIDIA CORPORATION.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the NVIDIA CORPORATION nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************/
 
 #include <cooperative_groups.h>
 #include <cooperative_groups/details/partitioning.h>
@@ -74,6 +38,9 @@ using namespace cooperative_groups;
 #define G_TBLOCK_LOG 11     // log2(gridDim.x)
 #define G_HIST_PART_SIZE \
   (size >> G_TBLOCK_LOG)  // Partition tile size in k_GlobalHistogram
+
+// these two macros need to be replaced with
+
 #define G_HIST_PART_START \
   (blockIdx.x * G_HIST_PART_SIZE)  // Starting offset of a partition tile
 #define G_HIST_PART_END \
